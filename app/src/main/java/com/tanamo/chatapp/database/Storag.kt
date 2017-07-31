c5798fae3d7a707fc370693f76f9d1be0c043be6
Unknown <tanamoinc@gmail.com>
2017-07-31 17:21:50 +0000

    commit

---
+++ app/src/main/java/com/tanamo/chatapp/database/Storag.kt
@@ -0,0 +1,7 @@
+package com.tanamo.chatapp.database
+
+/**
+ * Created by ${TANDOH} on ${6/20/2017}.
+ */
+class Storag {
+}
---
+++ app/src/main/java/com/tanamo/chatapp/model/mod.kt
@@ -0,0 +1,11 @@
+package com.tanamo.chatapp.model
+
+/**
+ * Created by ${TANDOH} on ${6/20/2017}.
+ */
+class mod {
+
+
+
+
+}
